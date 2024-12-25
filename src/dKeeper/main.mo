c2da54f9580678c77d3cc5f36fd8457852b47157
Tharanga Anuradha <104713256+TA-rathnayaka@users.noreply.github.com>
2024-12-25 13:29:11 +0530

    created desentralized keeper app

--- src/dKeeper/main.mo
+++ src/dKeeper/main.mo
@@ -1,3 +1,33 @@
+import Text "mo:base/Text";
+import List "mo:base/List";
+import Debug "mo:base/Debug";
+import Nat "mo:base/Nat";
+
 actor DKeeper{
+  public type Note = {
+    title:Text;
+    content:Text;
+  };
+  stable var notes:List.List<Note> = List.nil<Note>();
+
+  public func createNote(titleText:Text, contentText:Text){
+    let newNote: Note = {
+      title=titleText;
+      content=contentText;
+    };
+    notes := List.push(newNote, notes);
+    Debug.print(debug_show(notes));
+  };
+
+  public query func readNotes(): async  [Note]{
+    return List.toArray(notes);
+  };
+  public func removeNote(id:Nat) {
+ 
+      notes := List.append<Note>(
+        List.take<Note>(notes, id), 
+        List.drop<Note>(notes, id+1)
+      );
   
+  };
 };
--- src/dKeeper_assets/src/components/App.jsx
+++ src/dKeeper_assets/src/components/App.jsx
@@ -1,20 +1,33 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
 import CreateArea from "./CreateArea";
+import "../../../declarations/dKeeper";
+import { dKeeper } from "../../../declarations/dKeeper";
 
 function App() {
   const [notes, setNotes] = useState([]);
 
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  async function fetchData() {
+    const notes = await dKeeper.readNotes();
+    setNotes(notes);
+  }
+
   function addNote(newNote) {
-    setNotes(prevNotes => {
-      return [...prevNotes, newNote];
+    setNotes((prevNotes) => {
+      dKeeper.createNote(newNote.title, newNote.content);
+      return [newNote, ...prevNotes];
     });
   }
 
   function deleteNote(id) {
-    setNotes(prevNotes => {
+    setNotes((prevNotes) => {
+      dKeeper.removeNote(id);
       return prevNotes.filter((noteItem, index) => {
         return index !== id;
       });
